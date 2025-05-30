f4d36f15f29855df872df2ebdd731934b182e0e1
ashenglow <158246901+ashenglow@users.noreply.github.com>
2025-05-30 15:45:57 +0900

    Feature/test jenkins (#56)
    * fix: modify yml
    
    * fix: route
    
    * fix: modify script
    
    * Update run-all.sh
    
    * feat:add CorsWebFilter
    
    * fix: modify corsConfig
    
    * fix: add cors to yml
    
    * fix:modify securityConfig
    
    permit OPTION request

--- src/main/java/com/goormplay/memberservice/Security/SecurityConfig.java
+++ src/main/java/com/goormplay/memberservice/Security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.goormplay.memberservice.Security;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,7 +17,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(member -> member
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().permitAll() // 🔐 모든 요청 허용 (Gateway에서 이미 검증)
                 )
                 .addFilterBefore(new JwtParsingFilter(), UsernamePasswordAuthenticationFilter.class);
